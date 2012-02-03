2e7744998a489a4688a173bacc36043cfea368ef
Rudy Lippert <rudyl313@gmail.com>
2012-02-03 12:19:54 -0800

    Introduced some types and typeclasses

--- Main.hs
+++ Main.hs
@@ -14,19 +14,63 @@ main = do
   let oauthKey = head args
   let oauthSecret = last args
   let creds = Credentials oauthKey oauthSecret
-  let url = "http://api.v3.factual.com/t/places/read?limit=50&filters=%7B%22name%22%3A%22Bar%20Hayama%22%7D"
-  payload <- runQuery creds url
+  let query = TableQuery { table = Places
+                         , queryMethod = Read
+                         , limit = Just 50
+                         , filters = Just "%7B%22name%22%3A%22Bar%20Hayama%22%7D" }
+  putStrLn $ "http://api.v3.factual.com/" ++ toPath query
+  payload <- runQuery creds query
   putStrLn $ show payload
-
 
 
 data Credentials = Credentials String String
 
 
-runQuery :: Credentials -> String -> IO Value
-runQuery credentials url = do
+data Table = Places | USRestaurants | Global deriving Eq
+instance Show Table where
+  show Places = "places"
+  show USRestaurants = "restaurants-us"
+  show Global = "global"
+
+data QueryMethod = Read | Schema deriving Eq
+instance Show QueryMethod where
+  show Read = "read"
+  show Schema = "schema"
+
+
+
+class Query q where
+  toPath :: q -> String
+
+data TableQuery = TableQuery { table :: Table
+                             , queryMethod :: QueryMethod
+                             , limit :: Maybe Int
+                             , filters :: Maybe String
+                             } deriving (Eq, Show)
+
+instance Query TableQuery where
+  toPath q = "t/"
+           ++ (show $ table q)
+           ++ "/"
+           ++ (show $ queryMethod q)
+           ++ "?"
+           ++ (limitString $ limit q)
+           ++ (filtersString $ filters q)
+
+limitString :: Maybe Int -> String
+limitString (Just x) = "limit=" ++ show x ++ "&"
+limitString Nothing = ""
+
+filtersString :: Maybe String -> String
+filtersString (Just s) = "filters=" ++ s
+filtersString Nothing = ""
+
+
+
+runQuery :: (Query q) => Credentials -> q -> IO Value
+runQuery credentials query = do
   let token = generateToken credentials
-  let request = generateRequest url
+  let request = generateRequest ("http://api.v3.factual.com/" ++ toPath query)
   response <- runOAuthM token $ setupOAuth request
   return $ extractJSON response
 
