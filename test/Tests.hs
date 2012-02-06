2ebe468ab6af7ffeec38e6a95d2eec2406efcfa9
Rudy Lippert <rudyl313@gmail.com>
2012-02-06 11:31:52 -0800

    Added a couple more test cases

--- test/Tests.hs
+++ test/Tests.hs
@@ -3,17 +3,30 @@ import Data.Factual.Query
 import Data.Factual.Table
 import Data.Factual.ReadQuery
 
-tests = TestList [ TestLabel "Table test" table_test ]
+placeTableTest = TestCase (do let query = blankReadQuery { table = Places }
+                              let path = toPath query
+                              assertEqual "Correct table" path "/t/places/read?include_count=false")
 
-table_test = TestCase (do let query = ReadQuery { table = Places
-                                                , search = AndSearch []
-                                                , select = []
-                                                , limit = Nothing
-                                                , offset = Nothing
-                                                , filters = []
-                                                , geo = Nothing
-                                                , includeCount = False }
-                          let path = toPath query
-                          assertEqual "Correct table" path "/t/places/read?include_count=false")
+restaurantsTableTest = TestCase (do let query = blankReadQuery { table = USRestaurants }
+                                    let path = toPath query
+                                    assertEqual "Correct table" path "/t/restaurants-us/read?include_count=false")
+
+globalTableTest = TestCase (do let query = blankReadQuery { table = Global }
+                               let path = toPath query
+                               assertEqual "Correct table" path "/t/global/read?include_count=false")
+
+blankReadQuery :: ReadQuery
+blankReadQuery = ReadQuery { table = Places
+                           , search = AndSearch []
+                           , select = []
+                           , limit = Nothing
+                           , offset = Nothing
+                           , filters = []
+                           , geo = Nothing
+                           , includeCount = False }
+
+tests = TestList [ TestLabel "Place table test" placeTableTest
+                 , TestLabel "Restaurants table test" restaurantsTableTest
+                 , TestLabel "Global table test" globalTableTest ]
 
 main = runTestTT tests
