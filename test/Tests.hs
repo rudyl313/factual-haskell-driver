934b7d1077b13c151a1bd370ff62cbbabca40980
Rudy Lippert <rudyl313@gmail.com>
2012-02-08 10:33:17 -0800

    Added a few more tests

--- test/Tests.hs
+++ test/Tests.hs
@@ -3,17 +3,42 @@ import Data.Factual.Query
 import Data.Factual.Table
 import Data.Factual.ReadQuery
 
-placeTableTest = TestCase (do
+placeTablePathTest = TestCase (do
   let expected = "/t/places/read?include_count=false"
   assertEqual "Correct path for places table" (pathForTable Places) expected)
 
-restaurantsTableTest = TestCase (do
+restaurantsTablePathTest = TestCase (do
   let expected = "/t/restaurants-us/read?include_count=false"
   assertEqual "Correct path for us restaurants table" (pathForTable USRestaurants) expected)
 
-globalTableTest = TestCase (do
+globalTablePathTest = TestCase (do
   let expected = "/t/global/read?include_count=false"
   assertEqual "Correct path for global table" (pathForTable Global) expected)
+
+andSearchPathTest = TestCase (do
+  let expected = "/t/places/read?q=foo bar&include_count=false"
+  let path = toPath $ blankReadQuery { search = AndSearch ["foo", "bar"] }
+  assertEqual "Correct path for ANDed search" path expected)
+
+orSearchPathTest = TestCase (do
+  let expected = "/t/places/read?q=foo,bar&include_count=false"
+  let path = toPath $ blankReadQuery { search = OrSearch ["foo", "bar"] }
+  assertEqual "Correct path for ANDed search" path expected)
+
+selectPathTest = TestCase (do
+  let expected = "/t/places/read?select=foo,bar&include_count=false"
+  let path = toPath $ blankReadQuery { select = ["foo", "bar"] }
+  assertEqual "Correct path for select terms" path expected)
+
+limitPathTest = TestCase (do
+  let expected = "/t/places/read?limit=321&include_count=false"
+  let path = toPath $ blankReadQuery { limit = Just 321 }
+  assertEqual "Correct path for limit" path expected)
+
+offsetPathTest = TestCase (do
+  let expected = "/t/places/read?offset=321&include_count=false"
+  let path = toPath $ blankReadQuery { offset = Just 321 }
+  assertEqual "Correct path for offset" path expected)
 
 pathForTable :: Table -> String
 pathForTable = toPath . readQueryForTable
@@ -31,8 +56,13 @@ blankReadQuery = ReadQuery { table = Places
                            , geo = Nothing
                            , includeCount = False }
 
-tests = TestList [ TestLabel "Place table test" placeTableTest
-                 , TestLabel "Restaurants table test" restaurantsTableTest
-                 , TestLabel "Global table test" globalTableTest ]
+tests = TestList [ TestLabel "Place table test" placeTablePathTest
+                 , TestLabel "Restaurants table test" restaurantsTablePathTest
+                 , TestLabel "Global table test" globalTablePathTest
+                 , TestLabel "And search test" andSearchPathTest
+                 , TestLabel "Or search test" orSearchPathTest
+                 , TestLabel "Select test" selectPathTest
+                 , TestLabel "Limit test" limitPathTest
+                 , TestLabel "Offset test" offsetPathTest ]
 
 main = runTestTT tests
