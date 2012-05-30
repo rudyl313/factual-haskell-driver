c9173c11d3919fa13e6e5431bc7a95b6777fceba
Rudy Lippert <rudyl313@gmail.com>
2012-05-29 21:59:26 -0700

    Added addition table options

--- Data/Factual/Shared/Table.hs
+++ Data/Factual/Shared/Table.hs
@@ -6,16 +6,25 @@ module Data.Factual.Shared.Table
      Table(..)
   ) where
 
--- | Three Table values are currently available for the Factual API.
+-- | This type defines the available tables. Use the Custom table option for
+--   tables that are not listed you.
 data Table = Places
            | USRestaurants
            | Global
+           | HealthCareProviders
+           | WorldGeographies
+           | ProductsCPG
+           | ProductsCrosswalk
            | Custom String
            deriving Eq
 
 -- Table is a member of the Show typeclass to generate the beginning of the path.
 instance Show Table where
-  show Places        = "/t/places/"
-  show USRestaurants = "/t/restaurants-us/"
-  show Global        = "/t/global/"
-  show (Custom name) = "/t/" ++ name ++ "/"
+  show Places              = "/t/places/"
+  show USRestaurants       = "/t/restaurants-us/"
+  show Global              = "/t/global/"
+  show HealthCareProviders = "/t/health-care-providers-us"
+  show WorldGeographies    = "/t/world-geographies"
+  show ProductsCPG         = "/t/products-cpg"
+  show ProductsCrosswalk   = "/t/products-crosswalk"
+  show (Custom name)       = "/t/" ++ name ++ "/"
