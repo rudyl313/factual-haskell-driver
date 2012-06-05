ae81a6602366d901b181fe72766a9f1baeda5f03
Rudy Lippert <rudyl313@gmail.com>
2012-06-04 22:23:39 -0700

    Version bump

--- Network/Factual/API.hs
+++ Network/Factual/API.hs
@@ -106,7 +106,7 @@ extractJSON :: Response -> Value
 extractJSON = fromJust . decode . rspPayload
 
 headersList :: [(String, String)]
-headersList = [("X-Factual-Lib", "factual-haskell-driver-0.2.0")]
+headersList = [("X-Factual-Lib", "factual-haskell-driver-0.3.0")]
 
 basePath :: String
 basePath = "http://api.v3.factual.com"
